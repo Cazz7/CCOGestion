5e0e4038184ff318862b5fcb182cd5dab45a9009
cazz182 <cazz182@gmail.com>
2017-09-16 11:05:43 -0500

    Se eliminan comentarios de FirebaseInstanceIDService

--- app/src/main/java/com/cco/cristiancarlosjohn/ccogestion/WEB/FirebaseInstanceIDService.java
+++ app/src/main/java/com/cco/cristiancarlosjohn/ccogestion/WEB/FirebaseInstanceIDService.java
@@ -26,51 +26,6 @@ public class FirebaseInstanceIDService extends FirebaseInstanceIdService {
         super.onTokenRefresh();
 
         FirebaseMessaging.getInstance().subscribeToTopic("test1");
-        //String token = FirebaseInstanceId.getInstance().getToken();
         FirebaseInstanceId.getInstance().getToken();
-        //String token = "HOLAAMIGO2";
-        //Send token into the DB
-        //if ((!TextUtils.isEmpty(token) && (token != null)) && (!TextUtils.isEmpty(usuario) && (usuario != null) )
-        //        && (!TextUtils.isEmpty(perfil) && (perfil != null) ) )
-
-                //registerToken(token, usuario, perfil);
     }
-
-    // Token received
-
-//    public void onTokenRefresh(String usuario, String perfil) {
-//        super.onTokenRefresh();
-//
-//        FirebaseMessaging.getInstance().subscribeToTopic("test1");
-//        //String token = FirebaseInstanceId.getInstance().getToken();
-//        FirebaseInstanceId.getInstance().getToken();
-//        //String token = "HOLAAMIGO2";
-//        //Send token into the DB
-//        //if ((!TextUtils.isEmpty(token) && (token != null)) && (!TextUtils.isEmpty(usuario) && (usuario != null) )
-//        //        && (!TextUtils.isEmpty(perfil) && (perfil != null) ) )
-//
-//                //registerToken(token, usuario, perfil);
-//    }
-
-//    private void registerToken(String token, String usuario, String perfil) {
-//
-//        //Luego se hará sin volley
-//        OkHttpClient client = new OkHttpClient();
-//        RequestBody body = new FormBody.Builder()
-//                .add("token",token)
-//                .add("usuario",usuario)
-//                .add("perfil",perfil)
-//                .build();
-//
-//        Request request = new Request.Builder()
-//                .url(Constantes.REGISTER_TOKEN)
-//                .post(body)
-//                .build();
-//
-//        try {
-//            client.newCall(request).execute();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
 }
