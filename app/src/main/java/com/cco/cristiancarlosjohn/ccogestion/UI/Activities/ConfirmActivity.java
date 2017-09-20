7d6fe18889cebf475843df3d8e297107d26c40ae
Carlos <carlos.cortinezz@gmail.com>
2017-09-19 21:14:32 -0500

    Merge remote-tracking branch 'remotes/origin/BotonesConfirmacion' into InsertFragment_Carlos
    # Conflicts:
    #	app/src/main/java/com/cco/cristiancarlosjohn/ccogestion/UI/Activities/ConfirmActivity.java
    #	app/src/main/java/com/cco/cristiancarlosjohn/ccogestion/UI/Fragments/InsertFragment.java

--- app/src/main/java/com/cco/cristiancarlosjohn/ccogestion/UI/Activities/ConfirmActivity.java
+++ app/src/main/java/com/cco/cristiancarlosjohn/ccogestion/UI/Activities/ConfirmActivity.java
@@ -312,7 +312,7 @@ public class ConfirmActivity extends AppCompatActivity implements LocationDialog
 
         final Calendar c = Calendar.getInstance();
         int year = c.get(Calendar.YEAR);
-        int month = c.get(Calendar.MONTH);
+        int month = c.get(Calendar.MONTH)+1;
         int day = c.get(Calendar.DAY_OF_MONTH);
         int hour = c.get(Calendar.HOUR_OF_DAY);
         int minute = c.get(Calendar.MINUTE);
